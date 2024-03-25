3a37e6f906b0d715700763f9f98501e935e6ad26
Prajay <prajay090@gmail.com>
2024-03-25 15:59:01 -0400

    Card Flip

--- src/components/Ocassion/Ocassion.jsx
+++ src/components/Ocassion/Ocassion.jsx
@@ -1,37 +1,59 @@
-import './Ocassion.css'
+import './Ocassion.css' 
 import image1 from './image-1.png'
 import image2 from './image-2.png'
 import image3 from './image-3.png'
 import image4 from './image-4.png'
-import { useState } from 'react'
 function Ocassion()
 {
-    const [flipped, setFlipped] = useState(false);
-
-    const handleClick = () => {
-      setFlipped(!flipped);
-    };
  return(
-    <div className="Ocassion">
-    <div className={`cards${flipped ? 'flipped' : ''}`}>
-    <div className='card'>
-    <h2>Formal events</h2>
-    <img src={image1}></img>
-    </div>
-    <div className="card">
-    <h2>Casual Events</h2>
-    <img src={image2}></img>
-    </div>
-    <div className="card">
-    <h2>Social Events</h2>
-    <img src={image3}></img>
-    </div>
-    <div className="card">
-    <h2>Seasonal Events</h2>
-    <img src={image4}></img>
-    </div>
-    </div>
-    </div>
+   <div className= "flipcards">
+      <div className="container">
+         <div className="cardd">  
+            <div className="front">
+            <h2>Formal events</h2>
+            <img class = "img" src={image1}></img>
+            </div>
+            <div className="back">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, a?</p>
+            </div>
+         </div>
+      </div>
+      <div className="container">
+         <div className="cardd">  
+            <div className="front">
+            <h2>Casual events</h2>
+            <img class = "img" src={image2}></img>
+            </div>
+            <div className="back">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, a?</p>
+            </div>
+         </div>
+      </div>
+      <div className="container">
+         <div className="cardd">  
+            <div className="front">
+            <h2>Social events</h2>
+            <img class = "img" src={image3}></img>
+            </div>
+            <div className="back">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, a?</p>
+            </div>
+         </div>
+      </div>
+      <div className="container">
+         <div className="cardd">  
+            <div className="front">
+            <h2>Seasonal events</h2>
+            <img class = "img" src={image4}></img>
+            </div>
+            <div className="back">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, a?</p>
+            </div>
+         </div>
+      </div>
+   </div>
+
+
  )
 }
 export default Ocassion
