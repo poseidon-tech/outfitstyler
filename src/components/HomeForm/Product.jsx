e310dc4e3539dd00c5cd0b6805af755ecbe954b8
Prajay <prajay090@gmail.com>
2024-04-21 10:13:45 -0400

    modified product.jsx

--- src/components/HomeForm/Product.jsx
+++ src/components/HomeForm/Product.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ReactGA from "react-ga4";
+import { useEffect } from 'react';
 
 export default function Product(props) {
+  const handleLinkClick = () => {
+    // Track link click event
+    ReactGA.event({
+      category: "User Interaction",
+      action: "Clicked Link",
+      label: props.name, // Optional
+    });}
   return (
     <div className="card">
       <img className="product--image" src={props.url} alt="product image" />
       <h2>{props.name}</h2>
       <p className="price">{props.price}</p>
       <p>
-      <Link to={props.urll}><button>Try It</button></Link>
+      <Link to={props.urll} onClick={handleLinkClick}><button>Try It</button></Link>
       </p>
     </div>
   );
